322a8efe75f088a29f5c888506facdab438aebe2
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-10-09 15:24:09 +0300

    add getSimilarProducts method to ProductController, ProductService

--- src/main/java/ua/example/online_store/web/controller/ProductController.java
+++ src/main/java/ua/example/online_store/web/controller/ProductController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +35,13 @@ public class ProductController {
         .map(productMapper::toDto));
   }
 
+  @GetMapping("/similar_products/{id}")
+  public ResponseEntity<Page<ProductDto>> getSimilarProducts(
+      @PageableDefault(size = 10, page = 0, sort = "price.value",
+          direction = Direction.ASC) Pageable pageable,
+      @PathVariable Long id) {
+    log.info("invoked method {}", "getSimilarProducts()");
+    return ResponseEntity.ok(productService.getSimilarProducts(pageable, id)
+        .map(productMapper::toDto));
+  }
 }
