7626bc5da8f80c7c79c7ef02d356d3bcce1ddcc8
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-10-11 07:28:06 +0300

    add cron time zone, set from field in SubscribingEmailService, add price of SKU in email message

--- src/main/java/ua/example/online_store/service/email/SubscribingEmailService.java
+++ src/main/java/ua/example/online_store/service/email/SubscribingEmailService.java
@@ -4,6 +4,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -15,21 +16,23 @@ public class SubscribingEmailService implements EmailService {
 
   private final JavaMailSender mailSender;
 
+  @Value("${spring.mail.username}")
+  private String sender;
+
   @Override
   public void sendEmail(String email, String title, String textMessage) {
 
     MimeMessage message = mailSender.createMimeMessage();
-
     try {
       MimeMessageHelper helper = new MimeMessageHelper(message, true);
+      helper.setFrom(String.format("Online store project <%s>", sender));
       helper.setTo(email);
       helper.setSubject(title);
       helper.setText(textMessage);
+      mailSender.send(message);
+      log.info("Email to {} sent successfully", email);
     } catch (MessagingException e) {
       log.error("Failed to send email to {}", email, e);
-      return;
     }
-    mailSender.send(message);
-    log.info("Email to {} sent successfully", email);
   }
 }
