8b56ad0407422ea134dc7add77eae567954fb21c
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-11-01 07:17:10 +0200

    change RequestParam to RequestBody in SubscriptionController

--- src/main/java/ua/example/online_store/web/controller/subscription/SubscriptionController.java
+++ src/main/java/ua/example/online_store/web/controller/subscription/SubscriptionController.java
@@ -1,7 +1,6 @@
 package ua.example.online_store.web.controller.subscription;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,8 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.webjars.NotFoundException;
@@ -19,6 +18,7 @@ import ua.example.online_store.model.subscription.Subscriber;
 import ua.example.online_store.model.subscription.Subscription;
 import ua.example.online_store.service.subscription.SubscriberService;
 import ua.example.online_store.service.subscription.SubscriptionService;
+import ua.example.online_store.web.dto.subscription.SubscriberDto;
 
 @Slf4j
 @RestController
@@ -37,26 +37,24 @@ public class SubscriptionController {
   }
 
   @PostMapping
-  public ResponseEntity<Subscriber> subscribe(@RequestParam Long subscriptionId,
-      @Valid @Email @RequestParam String email) {
+  public ResponseEntity<Subscriber> subscribe(@Valid @RequestBody SubscriberDto subscriberDto) {
     log.info(INVOKED_METHOD, "subscribe()");
     Subscriber subscriber = subscriberService.findAllBySubscriptionIdAndEmail(
-            subscriptionId, email)
+            subscriberDto.getSubscriptionId(), subscriberDto.getEmail())
         .orElse(Subscriber.builder()
-            .subscription(subscriptionService.findById(subscriptionId)
+            .subscription(subscriptionService.findById(subscriberDto.getSubscriptionId())
                 .orElseThrow(() -> new NotFoundException("subscription not found")))
-            .email(email)
+            .email(subscriberDto.getEmail())
             .build());
     return ResponseEntity.ok(subscriberService.save(subscriber));
   }
 
   @DeleteMapping
   @ResponseStatus(HttpStatus.OK)
-  public void unsubscribe(@RequestParam Long subscriptionId,
-      @Valid @Email @RequestParam String email) {
+  public void unsubscribe(@Valid @RequestBody SubscriberDto subscriberDto) {
     log.info(INVOKED_METHOD, "unsubscribe()");
     Subscriber subscriber = subscriberService.findAllBySubscriptionIdAndEmail(
-            subscriptionId, email)
+            subscriberDto.getSubscriptionId(), subscriberDto.getEmail())
         .orElseThrow(() -> new NotFoundException("subscriber not found"));
     subscriberService.delete(subscriber);
   }
