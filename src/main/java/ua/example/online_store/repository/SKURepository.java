4e2b282fb04680749f3af85e4fa495c307d940a2
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-11-21 09:53:42 +0200

    add join fetch in ProductRepository and SKURepository

--- src/main/java/ua/example/online_store/repository/SKURepository.java
+++ src/main/java/ua/example/online_store/repository/SKURepository.java
@@ -2,10 +2,14 @@ package ua.example.online_store.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ua.example.online_store.model.SKU;
 
 public interface SKURepository extends JpaRepository<SKU, Long> {
 
-  List<SKU> findAllByStatus(boolean status);
+  @Query("FROM SKU s JOIN FETCH s.characteristics JOIN FETCH s.product "
+      + "WHERE s.status = :status")
+  List<SKU> findAllByStatus(@Param("status") boolean status);
 
 }
