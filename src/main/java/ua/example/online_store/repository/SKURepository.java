69f09b8e359a13263272bb34a9ac330404130682
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-11-21 10:05:05 +0200

    fix query in SKURepository

--- src/main/java/ua/example/online_store/repository/SKURepository.java
+++ src/main/java/ua/example/online_store/repository/SKURepository.java
@@ -8,7 +8,7 @@ import ua.example.online_store.model.SKU;
 
 public interface SKURepository extends JpaRepository<SKU, Long> {
 
-  @Query("FROM SKU s JOIN FETCH s.characteristics JOIN FETCH s.product "
+  @Query("FROM SKU s JOIN FETCH s.product "
       + "WHERE s.status = :status")
   List<SKU> findAllByStatus(@Param("status") boolean status);
 
