ecb870e791488b553c9dd642bf5331557ddfbe45
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-11-23 08:18:23 +0200

    add EntityGraphType.LOAD

--- src/main/java/ua/example/online_store/repository/SKURepository.java
+++ src/main/java/ua/example/online_store/repository/SKURepository.java
@@ -1,11 +1,14 @@
 package ua.example.online_store.repository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.example.online_store.model.SKU;
 
 public interface SKURepository extends JpaRepository<SKU, Long> {
 
+  @EntityGraph(attributePaths = {"product"}, type = EntityGraphType.LOAD)
   List<SKU> findAllByStatus(boolean status);
 
 }
