2c03d930040e43e5e328f2feeaff4a781eed2f55
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-11-24 09:52:38 +0200

    add findByTitle, change description type

--- src/main/java/ua/example/online_store/repository/CategoryRepository.java
+++ src/main/java/ua/example/online_store/repository/CategoryRepository.java
@@ -1,8 +1,10 @@
 package ua.example.online_store.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.example.online_store.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+  Optional<Category> findByTitle(String title);
 }
